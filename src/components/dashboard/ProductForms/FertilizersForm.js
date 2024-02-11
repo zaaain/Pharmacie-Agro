fca9328060f85dd452c7720b7cfcd3117bcd08ec
Zainoye <zain@geekgenix.com>
2024-02-12 01:09:16 +0500

    f

--- src/components/dashboard/ProductForms/FertilizersForm.js
+++ src/components/dashboard/ProductForms/FertilizersForm.js
@@ -1,11 +1,262 @@
-import React from 'react'
+import React from "react";
+import FormInput from "components/common/base/FormInput";
+import SelectInput from "components/common/base/SelectInput";
+import TextAreaInput from "components/common/base/TextAreaInput";
+import DateInput from "components/common/base/DateInput"
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm, Controller } from "react-hook-form";
+import {
+  packagingType,
+  weightUnitType,
+  yesNoOption,
+  taxOpt,
+  shippingOption,
+} from "helpers/constant";
+import { Button } from "components/common/base/button";
+import ImageInput from "components/common/base/ImageInput";
+import { FruitsFormSchema } from "helpers/schema";
 
-const FertilizersForm = () => {
+const FertilizersForm = ({ onSubmit }) => {
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(FruitsFormSchema),
+  });
+
   return (
-    <div>
-      FertilizersForm
-    </div>
-  )
-}
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="grid grid-cols-6 gap-4 items-center">
+        <div className="col-span-3">
+          <Controller
+            name="name"
+            control={control}
+            render={({ field }) => (
+              <FormInput
+                {...register("name")}
+                placeholder="Brand Name"
+                value={field.value}
+                onChange={(e) => field.onChange(e.target.value)}
+                error={errors?.name && errors.name.message}
+              />
+            )}
+          />
+        </div>
+        <div className="col-span-3">
+          <Controller
+            name="pkgType"
+            control={control}
+            defaultValue={null}
+            render={({ field }) => (
+              <FormInput
+                onChange={(selectedOption) => field.onChange(selectedOption)}
+                options={packagingType}
+                placeholder="Product Name"
+                value={field.value}
+                error={errors?.pkgType && errors.pkgType.message}
+              />
+            )}
+          />
+        </div>
+        <div className="col-span-3">
+          <Controller
+            name="pkgType"
+            control={control}
+            defaultValue={null}
+            render={({ field }) => (
+              <FormInput
+                onChange={(selectedOption) => field.onChange(selectedOption)}
+                options={packagingType}
+                placeholder="Chemical Name"
+                value={field.value}
+                error={errors?.pkgType && errors.pkgType.message}
+              />
+            )}
+          />
+        </div>
+        <div className="col-span-2">
+          <Controller
+            name="weightUnit"
+            control={control}
+            defaultValue={null}
+            render={({ field }) => (
+              <FormInput
+                onChange={(selectedOption) => field.onChange(selectedOption)}
+                options={weightUnitType}
+                placeholder="Enter Percentage"
+                value={field.value}
+                error={errors?.weightUnit && errors.weightUnit.message}
+              />
+            )}
+          />
+        </div>
+        <div className="grid grid-cols-1 justify-end">
+              <Button
+                width={140}
+                height={40}
+                value="Add Chemical"
+                variant="primary"
+              />
+        </div>
+        <div className="col-span-2">
+          <Controller
+            name="pkgWeight"
+            control={control}
+            render={({ field }) => (
+              <SelectInput
+                {...register("name")}
+                placeholder="Enter Package Weight"
+                value={field.value}
+                type="number"
+                onChange={(e) => field.onChange(e.target.value)}
+                error={errors?.pkgWeight && errors.pkgWeight.message}
+              />
+            )}
+          />
+        </div>
+        <div className="col-span-2">
+          <Controller
+            name="pkgQuantity"
+            control={control}
+            render={({ field }) => (
+              <SelectInput
+                {...register("name")}
+                placeholder="Packaging Type"
+                value={field.value}
+                type="number"
+                onChange={(e) => field.onChange(e.target.value)}
+                error={errors?.pkgQuantity && errors.pkgQuantity.message}
+              />
+            )}
+          />
+        </div>
+        <div className="col-span-2">
+          <Controller
+            name="price"
+            control={control}
+            render={({ field }) => (
+              <SelectInput
+                {...register("name")}
+                placeholder="Weight Unit"
+                value={field.value}
+                type="number"
+                onChange={(e) => field.onChange(e.target.value)}
+                error={errors?.price && errors.price.message}
+              />
+            )}
+          />
+        </div>
+        <div className="col-span-3">
+          <Controller
+            name="bidding"
+            control={control}
+            defaultValue={null}
+            render={({ field }) => (
+              <FormInput
+                onChange={(selectedOption) => field.onChange(selectedOption)}
+                options={yesNoOption}
+                placeholder="Packages Quantity"
+                value={field.value}
+                error={errors?.bidding && errors.bidding.message}
+              />
+            )}
+          />
+        </div>
+        <div className="col-span-3">
+          <Controller
+            name="tax"
+            control={control}
+            defaultValue={null}
+            render={({ field }) => (
+              <FormInput
+                onChange={(selectedOption) => field.onChange(selectedOption)}
+                options={taxOpt}
+                placeholder="Price per package"
+                value={field.value}
+                type="number"
+                error={errors?.tax && errors.tax.message}
+              />
+            )}
+          />
+        </div>
+        <div className="col-span-3">
+          <Controller
+            name="shipping"
+            control={control}
+            defaultValue={null}
+            render={({ field }) => (
+              <SelectInput
+                onChange={(selectedOption) => field.onChange(selectedOption)}
+                options={shippingOption}
+                placeholder="Select Shipping Type"
+                value={field.value}
+                error={errors?.shipping && errors.shipping.message}
+              />
+            )}
+          />
+        </div>
+        <div className="col-span-3">
+          <Controller
+            name="shipping"
+            control={control}
+            defaultValue={null}
+            render={({ field }) => (
+              <SelectInput
+                onChange={(selectedOption) => field.onChange(selectedOption)}
+                options={shippingOption}
+                placeholder="Tax"
+                value={field.value}
+                error={errors?.shipping && errors.shipping.message}
+              />
+            )}
+          />
+        </div>
+        <div className="col-span-6">
+          <Controller
+            name="description"
+            control={control}
+            render={({ field }) => (
+              <TextAreaInput
+                placeholder="Enter Product Description"
+                value={field.value}
+                onChange={(e) => field.onChange(e.target.value)}
+                error={errors?.description && errors.description.message}
+              />
+            )}
+          />
+        </div>
+        <div className="col-span-6">
+          <Controller
+            name="img"
+            control={control}
+            render={({ field }) => (
+              <ImageInput
+                onChange={(e) => {
+                  field.onChange(e);
+                }}
+                value={field.value}
+                error={errors?.img && errors.img.message}
+                {...register("img")}
+                placeholder="Enter Product Image"
+              />
+            )}
+          />
+        </div>
+        <div className="col-span-6 flex mx-auto">
+          <Button
+            value="Submit"
+            width={150}
+            height={45}
+            variant="primary"
+            type="submit"
+          />
+        </div>
+      </div>
+    </form>
+  );
+};
 
-export default FertilizersForm
+export default FertilizersForm;
+
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "layout/DashboardLayout"
+import Layout from "layout/DashboardLayout";
 import { Button } from "components/common/base/button";
 import FormInput from "components/common/base/FormInput";
 import TextAreaInput from "components/common/base/TextAreaInput";
@@ -8,20 +8,47 @@ import { imgUrl } from "helpers/path";
 const Profile = () => {
   return (
     <Layout>
-    <div className="p-4">
-      <div className="grid grid-cols-3 gap-5">
-        <div className="col-span-1 rounded-3xl border-2 border-primary p-5 flex flex-col  items-center justify-center">
-          <img
-            className="rounded-full mx-auto W-[150px] h-[150px]"
-            draggable={false}
-            src={imgUrl + "/kisan.png"}
-            alt="img"
-          />
-          <p className="font-CatamaranBold font-[24px] my-5">Customer Name</p>
-          <Button value="Update Avatar" width={220} height={50} />
-        </div>
-        <div className="col-span-2 flex flex-col justify-center items-center border-2 border-secondary rounded-3xl p-3">
-          <FormInput
+      <div className="p-4">
+        <div className="grid grid-cols-3 gap-5">
+          <div className="col-span-1 rounded-3xl border-2 border-primary p-5 flex flex-col  items-center justify-center">
+            <img
+              className="rounded-full mx-auto W-[150px] h-[150px]"
+              draggable={false}
+              src={imgUrl + "/kisan.png"}
+              alt="img"
+            />
+            <p className="font-CatamaranBold font-[24px] my-5">John Doe</p>
+            <Button value="Update Avatar" width={220} height={50} />
+          </div>
+          <div className="col-span-2 flex flex-col border-2 border-secondary rounded-3xl p-3">
+            <div className="grid grid-cols-2 gap-5">
+              <div className="col-span-1">
+                <FormInput placeholder="Enter Name" defaultValue="John Doe" />
+              </div>
+              <div className="col-span-1">
+                <FormInput
+                  placeholder="Enter Number"
+                  defaultValue="0300 0000000"
+                />
+              </div>
+              <div className="col-span-2">
+                <TextAreaInput
+                  rows={2}
+                  placeholder="Enter About"
+                  defaultValue="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+                />
+              </div>
+              <div className="col-span-2">
+                <div className="flex justify-center">
+                  <Button
+                    variant="primary"
+                    width={160}
+                    value="Update Bio"
+                    height={50}
+                  />
+                </div>
+              </div>
+              {/* <FormInput
             defaultValue="Khana Pull Rawalpindi, Pakistan"
             placeholder="Enter Address"
           />
@@ -31,10 +58,11 @@ const Profile = () => {
             width={150}
             height={50}
             variant="secondary"
-          />
+          /> */}
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="border-2 border-primary rounded-3xl mt-5 grid grid-cols-2 gap-2 p-3">
+        {/* <div className="border-2 border-primary rounded-3xl mt-5 grid grid-cols-2 gap-2 p-3">
         <div className="col-span-1">
           <FormInput placeholder="Enter Name" defaultValue="John Doe" />
         </div>
@@ -58,8 +86,54 @@ const Profile = () => {
             />
           </div>
         </div>
+      </div> */}
+      <div className="grid grid-cols-3 gap-5 mt-10">
+        <div className="col-span-3 flex justify-between items-center">
+          <p className="font-JosefinBold text-primary text-[28px]">My Location</p>
+          <Button
+                    variant="secondary"
+                    width={160}
+                    value="Add New Location"
+                    height={50}
+                  />
+        </div>
+
+      <div className="col-span-1 flex p-5 flex-col shadow-card  rounded-2xl">
+        <p className="font-Catamaran text-[16px]"><span className="font-Josefin text-primary text-[18px]">Addreess:</span> Rawalpindi Punjab Pakistan</p>
+        <div className="flex items-center justify-between mt-10">
+        <Button
+                    variant="primary"
+                    width={120}
+                    value="Update"
+                    height={40}
+                  />
+                          <Button
+                    variant="primary"
+                    width={120}
+                    value="Delete"
+                    height={40}
+                  />
+        </div>
       </div>
-    </div>
+      <div className="col-span-1 flex p-5 flex-col shadow-card  rounded-2xl">
+        <p className="font-Catamaran text-[16px]"><span className="font-Josefin text-primary text-[18px]">Addreess:</span> Faisalabad Punjab Pakistan</p>
+        <div className="flex items-center justify-between mt-10">
+        <Button
+                    variant="primary"
+                    width={120}
+                    value="Update"
+                    height={40}
+                  />
+                          <Button
+                    variant="primary"
+                    width={120}
+                    value="Delete"
+                    height={40}
+                  />
+        </div>
+      </div>
+      </div>
+      </div>
     </Layout>
   );
 };
