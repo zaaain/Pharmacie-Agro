2089de8d5c43ca3e7a213a621f9fd4325a1644cd
zaaain <contactzai254@gmail.com>
2024-02-14 04:50:41 -0800

    api

--- src/pages/MyProducts.js
+++ src/pages/MyProducts.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "layout/DashboardLayout"
 import ProductCard from "components/dashboard/ProductCard";
 import { Button } from "components/common/base/button";
 import { useNavigate } from "react-router-dom";
+import useClient from "hooks/useClient";
+import { CircularProgress } from "@mui/material";
 
 const Products = () => {
-  const data = [1, 2, , 3, , 4, 5, , 6, 7, 8, 9];
+
+  const [data, setData] = useState([])
+  const [loader,setLoader] = useState(false)
   const navigate = useNavigate()
+  const {api} = useClient()
+  const jwt = localStorage.getItem("jwt");
+
+
+  const handleGetMyProducts = () => {
+    const skip = data && data.length 
+    setLoader(true)
+    api.get(`/api/product/my?skip=${skip}`)
+    .then((res)=>{
+      console.log("myPro", res.data)
+      setLoader(false)
+    })
+    .catch((err)=>{
+      setLoader(false)
+    })
+  }
+
+  useEffect(()=>{
+    if(!jwt) return
+    handleGetMyProducts()
+  },[])
+
   return (
     <Layout>
       <div className="flex justify-center mb-2 z-10 bg-white sticky top-0 p-4"> 
@@ -20,9 +46,19 @@ const Products = () => {
       </div>
       <div className="p-4">
       <div className="grid grid-cols-3 gap-8">
-        {data.map((item) => (
+        {loader && (
+          <div className="col-span-3 flex items-center justify-center">
+            <CircularProgress size={36}/>
+          </div>
+        )}
+        {!loader && data && data.length === 0 && (
+          <div className="col-span-3 flex items-center justify-center">
+            <p className="font-Josefin text-[18px]">You have not added currenta any product !</p>
+          </div>
+        )}
+        {!loader && data && data.length > 0 && data.map((item) => (
           <div className="col-span-1" key={item}>
-            <ProductCard />
+            <ProductCard data={item}/>
           </div>
         ))}
       </div>
